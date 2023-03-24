3707bfdaf1f557353342b5dfe097bd83b09d50a0
Umbeee <umberto.nesta@hotmail.com>
2023-03-24 20:45:20 +0100

    esercizio finito

--- assets/main.js
+++ assets/main.js
@@ -13,5 +13,28 @@ function acquistaBiglietto(){
     document.getElementById('span-eta').innerHTML = fasciaEta;
 
     console.log(nome, km, fasciaEta);
+
+
+    const prezzoAlKm = 0.21;
+    let costo = km * prezzoAlKm;
+
+    if(fasciaEta == 'over_65'){
+        costo = costo * 0.6;
+        costo = costo.toFixed(2);
+    }else if(fasciaEta == 'minorenne'){
+        costo = costo * 0.8;
+        costo = costo.toFixed(2);
+        }else{
+        costo = costo.toFixed(2);
+    }
+    document.getElementById('span-costo').innerHTML = costo + ' €';
+
+    let carrozza = Math.floor(Math.random() * 10) + 1;
+
+    document.getElementById('span-carrozza').innerHTML = carrozza;
+
+    let codice = Math.floor(Math.random () * 10000) +1;
+
+    document.getElementById('span-codice').innerHTML = codice;
 }
 
